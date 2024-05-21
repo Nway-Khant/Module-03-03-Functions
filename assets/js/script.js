c3d591c907b972e1c91261077f4563de0246ca2e
nway khant <minkhantnway@gmail.com>
2024-05-21 20:53:24 +0630

    Hello

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function che (a1,a2){
+    if (a1===a2){
+        console.log("They are equal in type and value");
+    }
+
+    else if (a1==a2){
+        console.log("They are equal in value");
+
+        }
+    else {
+        console.log("The values are not equal");
+    }
+};
+
+che(12,"12");
